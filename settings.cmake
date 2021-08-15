dfe1e43e0b1a7515f41d9f97e050f4f52dea5c09
Grant Jurgensen <gajurgensen@gmail.com>
2021-08-15 11:23:57 -0500

    Init commit - based on cross_vm_connections example

---
+++ settings.cmake
@@ -0,0 +1,22 @@
+#
+# Copyright 2019, Data61, CSIRO (ABN 41 687 119 230)
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+set(supported "exynos5422;qemu-arm-virt")
+if(NOT "${PLATFORM}" IN_LIST supported)
+    message(FATAL_ERROR "PLATFORM: ${PLATFORM} not supported.
+         Supported: ${supported}")
+endif()
+set(VmPCISupport ON CACHE BOOL "" FORCE)
+set(LibUSB OFF CACHE BOOL "" FORCE)
+set(VmInitRdFile ON CACHE BOOL "" FORCE)
+if(${PLATFORM} STREQUAL "qemu-arm-virt")
+    # force cpu
+    set(QEMU_MEMORY "2048")
+    set(KernelArmCPU cortex-a53 CACHE STRING "" FORCE)
+endif()
+
+set(KernelNumDomains 2 CACHE STRING "" FORCE)
+set(KernelDomainSchedule "${CMAKE_CURRENT_LIST_DIR}/src/domain_schedule.c" CACHE INTERNAL "")
