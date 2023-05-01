25dfa870725bc4cdeb69e05a2c6294d853639028
Michael Neises <neisesmichael@gmail.com>
2023-05-01 18:14:52 -0500

    begin to intermingle measurement

--- components/Measurement/introspect_component.c
+++ components/Measurement/introspect_component.c
@@ -67,10 +67,37 @@ bool IsKernelRodataOkay()
     return result;
 }
 
+uint32_t fib_buf[10];
+
 int run(void)
 {
     ShaTest();
 
+
+    while (1) {
+        ready_wait();
+        printf("introspect: Got an event\n");
+
+        seL4_Word paddr = *(seL4_Word *)introspect_data;
+        printf("paddr in component 0x%x\n", paddr);
+
+        seL4_Word offset = paddr - RAM_BASE;
+
+        printf("offset in component 0x%x\n", offset);
+
+        memcpy(fib_buf, ((char *)memdev + offset), sizeof(uint32_t) * 10);
+
+        for (int i = 0; i < 10; i++) {
+            printf("camkes_fib[%d]@%p = %d, ", i, (fib_buf + i), fib_buf[i]);
+        }
+
+        printf("\n");
+        done_emit();
+    }
+
+
+
+    /*
     printf("\n\n\nDoing scanning tests...\n\n\n");
     for(int i=0; i<RAM_SIZE; i++)
     {
@@ -89,15 +116,18 @@ int run(void)
         }
     }
     printf("\n\n\nEnding scanning tests...\n\n\n");
+    */
 
     // Execution time without introspection: 0.66s
     // Time to Measure Modules: 0.01s
     // Time to Measure Tasks: 0.86s
     // Time to Measure Kernel Rodata: 0.53s
+    /* commented out 1 May 2023
     while (1)
     {
         printf("DEBUG: Measurement: Waiting.\n");
-        measurement_request_wait();
+        //measurement_request_wait();
+        ready_wait();
         bool appraisal = true;
         appraisal = appraisal && IsModulesOkay();
         appraisal = appraisal && IsTasksOkay();
@@ -105,10 +135,12 @@ int run(void)
         printf("DEBUG: Measurement: Overall Appraisal Result: %s\n", appraisal ? "Passed" : "Failed.");
         char* resultMsg = appraisal ? "1" : "0";
         // TODO hook the am back up
-        /* memset(ms_dp, '0', 4096); */
-        /* strcpy(ms_dp, resultMsg); */
-        measurement_done_emit();
+        //memset(ms_dp, '0', 4096);
+        //strcpy(ms_dp, resultMsg);
+        //measurement_done_emit();
+        done_emit();
     }
+    */
     return 0;
 }
 
