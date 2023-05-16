465e2eb86130be14c692beda5efe81fe1d01698c
Michael Neises <neisesmichael@gmail.com>
2023-05-16 00:44:14 -0500

    make linux version include abstractly-named

--- components/Measurement/configurations/include.h
+++ components/Measurement/configurations/include.h
@@ -1,4 +1,5 @@
-#include "linux-4.9.307.h"
+#include "linux_definitions.h"
+/* #include "linux-4.9.307.h" */
 /* #include "linux-4.14.314.h" */
 /* #include "linux-4.19.282.h" */
 /* #include "linux-5.10.179.h" */
