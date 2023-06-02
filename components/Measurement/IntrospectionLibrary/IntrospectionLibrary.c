74cbf8b4b587226af6f9b8b7e5b2099d19ba09ee
Michael Neises <neisesmichael@gmail.com>
2023-06-02 18:05:36 -0500

    Fix address translation for Linux 5.4

--- components/Measurement/IntrospectionLibrary/IntrospectionLibrary.c
+++ components/Measurement/IntrospectionLibrary/IntrospectionLibrary.c
@@ -4,9 +4,9 @@
  * 30 May 2023
  */
 
-#include "Linux4-9/library.c"
+/* #include "Linux4-9/library.c" */
 /* #include "Linux4-19/library.c" */
-/* #include "Linux5-4/library.c" */
+#include "Linux5-4/library.c"
 /* #include "Linux5-10/library.c" */
 /* #include "Linux5-14/library.c" */
 /* #include "Linux6-1/library.c" */
