3a870b9f293264de9f7e73c9a6864fe7a6531b9e
Michael Neises <neisesmichael@gmail.com>
2023-06-02 18:03:47 -0500

    add helpful debugging information

--- modules/poison/poison.c
+++ modules/poison/poison.c
@@ -7,12 +7,17 @@
 #include <asm/uaccess.h>
 #include <asm/kvm_para.h>
 #include <asm/io.h>
+#include <asm/memory.h>
 
 static int __init poison_init(void)
 {
     printk("\n==============================\n");
     printk("Poison Module Init");
     printk("\n==============================\n");
+    printk("page offset is %llx \n", PAGE_OFFSET);
+    printk("page END is %llx \n", PAGE_END);
+    printk("kimage_vaddr is %llx \n", KIMAGE_VADDR);
+    printk("VA_BITS is %llx \n", VA_BITS);
     return 0;
 }
 
