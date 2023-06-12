4a8c6623111e17564d0ce627201b630334eea262
Michael Neises <neisesmichael@gmail.com>
2023-06-12 18:04:31 -0500

    fix example values

--- components/Measurement/IntrospectionLibrary/IntrospectionLibrary.c
+++ components/Measurement/IntrospectionLibrary/IntrospectionLibrary.c
@@ -4,10 +4,10 @@
  * 30 May 2023
  */
 
-/* #include "Linux4-9/library.c" */
-/* #include "Linux4-19/library.c" */
-/* #include "Linux5-4/library.c" */
-/* #include "Linux5-10/library.c" */
-/* #include "Linux5-14/library.c" */
-/* #include "Linux6-1/library.c" */
+/* #include "4.9.y/library.c" */
+/* #include "4.19.y/library.c" */
+/* #include "5.4.y/library.c" */
+/* #include "5.10.y/library.c" */
+/* #include "5.14.y/library.c" */
+/* #include "6.1.y/library.c" */
 
