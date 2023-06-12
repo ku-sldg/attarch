de50f1b8470a2bc036ca836a31655e6a19844101
Michael Neises <neisesmichael@gmail.com>
2023-06-12 18:13:40 -0500

    Revert "add debugging info"
    This reverts commit 53c6237b82b5c816866dc71bba5dbbe77276a5cd.

--- modules/poison/poison.c
+++ modules/poison/poison.c
@@ -14,10 +14,10 @@ static int __init poison_init(void)
     printk("\n==============================\n");
     printk("Poison Module Init");
     printk("\n==============================\n");
-    printk("page offset is %llx \n", PAGE_OFFSET);
+    /* printk("page offset is %llx \n", PAGE_OFFSET); */
     /* printk("page END is %llx \n", PAGE_END); */
-    printk("kimage_vaddr is %llx \n", KIMAGE_VADDR);
-    printk("VA_BITS is %llx \n", VA_BITS);
+    /* printk("kimage_vaddr is %llx \n", KIMAGE_VADDR); */
+    /* printk("VA_BITS is %llx \n", VA_BITS); */
     return 0;
 }
 
