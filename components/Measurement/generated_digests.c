7a96e118bd86f17ed09fb48ea7d1a99feeb35f64
Michael Neises <neisesmichael@gmail.com>
2023-06-12 13:04:30 -0500

    add requisite stub for pre-provisioning simulation

---
+++ components/Measurement/generated_digests.c
@@ -0,0 +1,8 @@
+/* Michael Neises */
+/* 12 June 2023 */
+/* Function Stub. It will be overwritten with a generated file during the provisioning step. */
+
+int register_digests(uint8_t* ignore)
+{
+    return 0;
+}
