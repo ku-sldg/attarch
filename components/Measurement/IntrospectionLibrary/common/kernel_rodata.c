8469064b772419c61cc2768fd410eb10606f4478
Michael Neises <neisesmichael@gmail.com>
2023-07-17 16:45:10 -0500

    Implement module and rodata measurements for linux 6.1.
    Again, 6.1 required the 0x80 page offset and the clarification of translation strategy at certain points. This was the same story as 5.15 and 5.10.

--- components/Measurement/IntrospectionLibrary/common/kernel_rodata.c
+++ components/Measurement/IntrospectionLibrary/common/kernel_rodata.c
@@ -8,30 +8,7 @@
 
 void MeasureKernelRodataPage(uint8_t* memory_device, uint8_t (*output_digest)[DIGEST_NUM_BYTES], uint64_t pageVaddr)
 {
-    uint64_t pagePaddr = 0;
-    if(pageVaddr < 0xFFFF800000000000)
-    {
-
-       /* This marks the virtual address as belonging to Linux 4.x.y */
-       /* So we will translate the address in a special way. */
-       /* In particular, we will simply subtract KIMAGE_VADDR, */
-       /*    which is the offset of the kernel in physical memory. */
-
-        pagePaddr = pageVaddr-INTRO_KIMAGE_VADDR; // this is simply vaddr - kimage_vaddr
-    }
-    else
-    {
-
-        /* Otherwise, we should handle this address in a different way. */
-        /*     In particular, we will subject it to the normal virtual address translation procedures. */
-        /* In Linux 5, the memory layout was changed so that kernel addresses were higher than PAGE_OFFSET. */
-        /*     I believe it was the opposite previous to that, otherwise it must be the case that the kernel */
-        /*     was translating its symbols in a way I've never read about. Perhaps the kernel addresses were always high, */
-        /*     except in the special case of the System.map addresses, which, despite being kernel addresses, */
-        /*     are for some reason stored as virtual addresses in the user range. */
-
-        pagePaddr = TranslateVaddr(memory_device, pageVaddr);
-    }
+    uint64_t pagePaddr = TranslateVaddr(memory_device, pageVaddr);
     HashMeasure( ((char*)memory_device+pagePaddr), INTRO_PAGE_SIZE, output_digest );
 }
 
@@ -62,7 +39,7 @@ void CollectRodataHashingAsWeGo(uint8_t* memory_device, uint8_t (*output_digest)
             printf("Skipping ro_after_init data.\n");
             continue;
         }
-        MeasureKernelRodataPage((char*)memory_device, (uint8_t (*) [DIGEST_NUM_BYTES])&((*digestArray)[i*DIGEST_NUM_BYTES]), thisPageVaddr);
+        MeasureKernelRodataPage((char*)memory_device, (uint8_t (*) [DIGEST_NUM_BYTES])&((*digestArray)[i*DIGEST_NUM_BYTES]), thisPageVaddr + 0x80000);
     }
     HashMeasure((uint8_t*)digestArray, NUM_RODATA_PAGES * DIGEST_NUM_BYTES, output_digest);
     free(digestArray);
