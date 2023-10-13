ebf9aaac55508817fe8d2cd1ec8415a48f0f3fd3
Michael Neises <neisesmichael@gmail.com>
2023-10-12 23:42:36 -0500

    Remove control from the inspector
    Let the inspector be a simple library, and let it expose certain RPC functions: measurement & appraisal. The inspector no longer has a thread of control. This greatly simplifies flow of control and removes all signal and dataport handling involving the inspector.

--- components/Measurement/introspect_component.c
+++ components/Measurement/introspect_component.c
@@ -11,72 +11,58 @@
 
 void introspective_measurement__init(void)
 {
-    printf("DEBUG: I'm about to give you your measurement\n");
+    printf("DEBUG: Here's the result of ShaTest()\n");
+    ShaTest();
 }
 
-bool introspective_measurement_request(int id)
+bool introspective_measurement_request(int id, char** evidence)
 {
     printf("DEBUG: here's your measurement id: %d\n", id);
-    return 0;
+    if(id==0)
+    {
+        bool overall_appraisal = MeasureAndAppraiseLinux();
+        *evidence = malloc(30);
+        for(int i=0; i<30; i++)
+        {
+            (*evidence)[i] = 'a';
+        }
+        (*evidence)[29] = '\0';
+        /* return overall_appraisal; */
+
+        /* evidence = malloc(10); */
+        /* (*evidence)[0] = 'A'; */
+        /* (*evidence)[1] = '\0'; */
+        return true;
+    }
+    else
+    {
+        printf("That measurement id does not exist.\n");
+        return false;
+    }
+    return false;
 }
 
-int run(void)
+bool introspective_measurement_appraise(int id, const char* evidence, char** appraisal_report)
 {
-    ShaTest();
-
-    printf("DEBUG: Inspector Awake. Waiting for Server signal\n");
-    measurement_request_wait();
-
-    char* msg_test = calloc(1,4096);
-    strcpy(msg_test, server_dp);
-    printf("recv from server: %s\n", strcmp(msg_test,"1") == 0 ? "Good" : "Failed");
-
-    msg_test = "3";
-    memset(server_dp, '0', 4096);
-    strcpy(server_dp, msg_test);
-    
-    msg_test = "4";
-    memset(client_dp, '0', 4096);
-    strcpy(client_dp, msg_test);
-
-    printf("DEBUG: Inspector recv Server signal. Emitting to Client.\n");
-    client_done_emit();
-    printf("DEBUG: Inspector waiting for Client signal\n");
-    client_ready_wait();
-    printf("DEBUG: Inspector recv Client signal. Emitting to Server.\n");
-
-    strcpy(msg_test, client_dp);
-    printf("recv from client: %s\n", msg_test);
-    printf("recv from client: %s\n", strcmp(msg_test,"6") == 0 ? "Good" : "Failed");
-
-    measurement_done_emit();
-
-
-    // Execution time without introspection: 0.66s
-    // Time to Measure Modules: 0.01s
-    // Time to Measure Tasks: 0.86s
-    // Time to Measure Kernel Rodata: 0.53s
-    while (1)
+    printf("DEBUG: here's your measurement id: %d\n", id);
+    printf("DEBUG: here's your evidence: %s\n", evidence);
+    if(id==0)
     {
+        bool result = MeasureAndAppraiseLinux();
 
+        *appraisal_report = malloc(30);
+        for(int i=0; i<30; i++)
+        {
+            (*appraisal_report)[i] = 'a';
+        }
+        (*appraisal_report)[29] = '\0';
 
-
-
-
-        printf("DEBUG: Inspector Start.\n");
-        /* ready_wait(); */
-        measurement_request_wait();
-
-        bool overall_appraisal = MeasureAndAppraiseLinux();
-
-        printf("DEBUG: Measurement: Overall Appraisal Result: %s\n", overall_appraisal ? "Passed" : "Failed.");
-        char* resultMsg = overall_appraisal ? "1" : "0";
-        // TODO hook the am back up
-        memset(server_dp, '0', 4096);
-        strcpy(server_dp, resultMsg);
-        /* done_emit(); */
-        measurement_done_emit();
+        return true;
     }
-    return 0;
+    else
+    {
+        printf("That measurement id does not exist.\n");
+        return false;
+    }
 }
 
