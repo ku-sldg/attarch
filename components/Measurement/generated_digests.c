f5c47288a6e7aa410a9d08680e8758f705dc6896
Michael Neises <neisesmichael@gmail.com>
2023-10-23 15:03:05 -0500

    update git ignores to exclude generated files

--- components/Measurement/generated_digests.c
+++
@@ -1,8 +0,0 @@
-/* Michael Neises */
-/* 12 June 2023 */
-/* Function Stub. It will be overwritten with a generated file during the provisioning step. */
-
-int register_digests(uint8_t* ignore)
-{
-    return 0;
-}
