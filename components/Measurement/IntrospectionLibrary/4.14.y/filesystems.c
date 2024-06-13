fb67c699e0227c9ef490e0693dde24bb621236a0
Michael Neises <neisesmichael@gmail.com>
2024-06-12 21:30:35 -0500

    Add stubs for build compatibility
    True FS measurement only for 5.4 right now, but do not block other versions of linux building.

---
+++ components/Measurement/IntrospectionLibrary/4.14.y/filesystems.c
@@ -0,0 +1,11 @@
+/*
+ * Filesystem  Interpretation and Measurement
+ * Michael Neises
+ * June 2024
+ * Measurement Stub
+ */
+
+void MeasureFileSystems(uint8_t* memory_device, uint8_t(*digest)[DIGEST_NUM_BYTES])
+{
+    return;
+}
