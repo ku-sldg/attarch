da3adf488109ca210518a85effcf8601cf64bb69
Michael Neises <neisesmichael@gmail.com>
2024-06-18 20:43:53 -0500

    Add Inode measurement stubs for each version of Linux

--- components/Measurement/IntrospectionLibrary/4.14.y/filesystems.c
+++ components/Measurement/IntrospectionLibrary/4.14.y/filesystems.c
@@ -5,7 +5,7 @@
  * Measurement Stub
  */
 
-void MeasureFileSystems(uint8_t* memory_device, uint8_t(*digest)[DIGEST_NUM_BYTES])
+void MeasureFileInode(uint8_t* memory_device, uint64_t inode_paddr, uint8_t(*digest)[DIGEST_NUM_BYTES])
 {
     return;
 }
