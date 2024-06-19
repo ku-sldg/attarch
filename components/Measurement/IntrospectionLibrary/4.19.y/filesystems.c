e90b57a4e43be52814f815dd43e779048e6b5c45
Michael Neises <neisesmichael@gmail.com>
2024-06-19 02:58:48 -0500

    add 4.19 definitions

--- components/Measurement/IntrospectionLibrary/4.19.y/filesystems.c
+++ components/Measurement/IntrospectionLibrary/4.19.y/filesystems.c
@@ -5,18 +5,18 @@
  * Measurement Stub
  */
 
-#define SUPER_BLOCK_S_TYPE 0
-#define SUPER_BLOCK_S_ROOT 0
-#define DENTRY_D_INODE 0
-#define DENTRY_D_NAME 0
-#define DENTRY_D_SUBDIRS 0
-#define DENTRY_D_CHILD 0
-#define INODE_I_DATA 0
-#define ADDRESS_SPACE_I_PAGES 0
+#define SUPER_BLOCK_S_TYPE 40
+#define SUPER_BLOCK_S_ROOT 104
+#define DENTRY_D_INODE 48
+#define DENTRY_D_NAME 32
+#define DENTRY_D_SUBDIRS 160
+#define DENTRY_D_CHILD 144
+#define INODE_I_DATA 376
+#define ADDRESS_SPACE_I_PAGES 8
 
 bool CanMeasureFileSystem()
 {
-    return false;
+    return true;
 }
 
 void MeasureFileInode(uint8_t* memory_device, uint64_t inode_paddr, uint8_t(*digest)[DIGEST_NUM_BYTES])
