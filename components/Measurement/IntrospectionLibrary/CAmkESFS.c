81c7a7efb4a563591287d6ba5823b04cb494f12b
Michael Neises <neisesmichael@gmail.com>
2024-06-21 00:55:27 -0500

    Clarify function ability and purpose

--- components/Measurement/IntrospectionLibrary/CAmkESFS.c
+++ components/Measurement/IntrospectionLibrary/CAmkESFS.c
@@ -1,28 +1,27 @@
 
+/* This function looks at the initial ramdisk. */
+/* It uses some CAmkES functions to do so. */
+/* It does not interact with the Linux VM. */
+/* It does not reflect the dynamic state of the Linux VM */
+/* It is not clear if it is useful. */
+
 void CheckFS()
 {
-    printf("\n\n\nchecking!\n");
-    
-    int fd = fs_open("linux-initrd", 0);
-
-    int sizeme = fs_get_size();
-    printf("size of fs is %d\n", sizeme);
-    int size = fs_read(fd, 1024);
-
-    for(int i=0; i<4096; i++)
-    {
-        if(i % 8 == 0)
-        {
-            printf("\t");
-        }
-        if(i % 64 == 0)
-        {
-            printf("\n");
-        }
-        printf("%02X", ((char*)fs_get_buf())[i]);
-    }
-
-
-
-    printf("\ndone checking!\n\n\n\n");
+    /* int fd = fs_open("linux-initrd", 0); */
+    /* int sizeme = fs_get_size(); */
+    /* printf("size of fs is %d\n", sizeme); */
+    /* int size = fs_read(fd, 1024); */
+    /* for(int i=0; i<4096; i++) */
+    /* { */
+    /*     if(i % 8 == 0) */
+    /*     { */
+    /*         printf("\t"); */
+    /*     } */
+    /*     if(i % 64 == 0) */
+    /*     { */
+    /*         printf("\n"); */
+    /*     } */
+    /*     printf("%02X", ((char*)fs_get_buf())[i]); */
+    /* } */
 }
+
